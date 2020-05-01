d9ec5fe653844ea851764dfb1e018f97a16ff5a2
prasanthteja <nvstprasanth@gmail.com>
2020-04-30 23:53:34 -0500

    workshops php

---
+++ Desktop/workshops/check.js
@@ -0,0 +1,18 @@
+function checkTheBox(method)
+{
+    var checkboxes = document.getElementsByName("registrants[]");
+    for(i=0; i < checkboxes.length; i++)
+    {
+        checkboxes[i].checked = method;
+    }
+}
+
+function changeSeats() {
+
+	var seatsReq = document.getElementsByName("seatsReq[]");
+
+	if(seatsReq[0].checked == true)
+		document.workshop.seats.disabled=false;
+	else
+		document.workshop.seats.disabled=true;
+}
